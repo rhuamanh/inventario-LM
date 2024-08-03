b02611d919d7665ab7cc8cada96c97c1bc7988cc
rhuamanh <rhuamanh@unjbg.edu.pe>
2024-08-03 15:46:12 -0500

    Definición de las estructuras segun los grupos de piezas de la ficha de inventario

---
+++ inventario.cpp
@@ -0,0 +1,177 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//Aqui definimos todas la estructuras segun la cantidad de grupos en la ficha de inventario
+struct gruposPiezas1{
+	int p4255563;
+	int p4177430;
+	int p4177431;
+}grupo1[1];
+
+struct gruposPiezas2{
+	int p4121715;
+}grupo2[1];
+
+struct gruposPiezas3{
+	int p4184169;
+	int p4107085;
+	int p4107783;
+}grupo3[1];
+
+struct gruposPiezas4{
+	int p4107767;
+	int p4515185;
+}grupo4[1];
+
+struct gruposPiezas5{
+	int p4107081;
+	int p4248204;
+	int p4143466;
+}grupo5[1];
+
+struct gruposPiezas6{
+	int p4121667;
+	int p4198367;
+}grupo6[1];
+
+struct gruposPiezas7{
+	int p4296059;
+}grupo7[1];
+
+struct gruposPiezas8{
+	int p4225033;
+}grupo8[1];
+
+struct gruposPiezas9{
+	int p4211865;
+	int p4211807;
+	int p4211483;
+}grupo9[1];
+
+struct gruposPiezas10{
+	int p4211889;
+	int p4211629;
+	int p4211880;
+}grupo10[1];
+
+struct gruposPiezas11{
+	int p4211775;
+	int p4211779;
+	int p4552348;
+}grupo11[1];
+
+struct gruposPiezas12{
+	int p4211622;
+	int p4211573;
+	int p4211375;
+}grupo12[1];
+
+struct gruposPiezas13{
+	int p4544140;
+	int p4281515;
+	int p306901;
+	int p4508664;
+}grupo13[1];
+
+struct gruposPiezas14{
+	int p4142865;
+	int p4206482;
+}grupo14[1];
+
+struct gruposPiezas15{
+	int p4514553;
+}grupo15[1];
+
+struct gruposPiezas16{
+	int p4514554;
+	int p4565452;
+	int p4186017;
+}grupo16[1];
+
+struct gruposPiezas17{
+	int p4119589;
+	int p4512360;
+	int p4210759;
+}grupo17[1];
+
+struct gruposPiezas18{
+	int p4535768;
+	int p4211639;
+	int p4211805;
+	int p4211815;
+	int p4211086;
+}grupo18[1];
+
+struct gruposPiezas19{
+	int p4545430;
+	int p4545434;
+	int p4545435;
+	int p4297031;
+}grupo19[1];
+
+struct gruposPiezas20{
+	int p370526;
+	int p4508553;
+	int p370626;
+	int p4141300;
+	int p370826;
+	int p4499858;
+}grupo20[1];
+
+struct gruposPiezas21{
+	int p4297210;
+	int p4184286;
+}grupo21[1];
+
+struct gruposPiezas22{
+	int p4537417;
+	int p4495932;
+	int p4297202;
+	int p4297200;
+	int p4522939;
+	int p4297197;
+}grupo22[1];
+
+struct gruposPiezas23{
+	int p4210751;
+	int p4210668;
+	int p4210667;
+	int p4234240;
+	int p4210753;
+	int p4211668;
+	int p4210638;
+	int p4211651;
+}grupo23[1];
+
+struct gruposPiezas24{
+	int p4297006;
+	int p4297174;
+	int p4296929;
+	int p4546542;
+}grupo24[1];
+
+struct gruposPiezas25{
+	int p4548553;
+	int p4547402;
+	int p4547403;
+	int p4288099;
+	int p4292468;
+	int p4502834;
+}grupo25[1];
+
+struct gruposPiezas26{
+	int p4297187;
+	int p4297188;
+	int p4297185;
+	string cable;
+}grupo26[1];
+
+struct gruposPiezas27{
+	int p4558295;
+}grupo27[1];
+
+struct gruposPiezas28{
+	int guia;
+	int test;
+	int disco;
+}grupo28[1];
